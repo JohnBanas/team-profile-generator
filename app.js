d7c463fa5fdec27eca72461aa385c5fdba7d9530
JPB <jbanas9124@gmail.com>
2021-04-14 17:55:28 -0500

    changed function to array of objects and added console.logs

--- app.js
+++ app.js
@@ -4,94 +4,99 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-newEmployeePrompt = () => {
-  inquirer
-    .prompt(
+newEmployeePrompt = [
+  
       {
         type: 'list',
-        name: 'type',
         message: 'What is your role in the team?',
+        name: 'type',
         choices: ['Manager', 'Engineer', 'Intern', 'Quit Application?']
       },
       {
         type: 'input',
-        name: 'name',
         message: 'What is the name of the individual?',
+        name: 'name',
         when: ({ type }) => type != 'Quit Application?',
-        validate: (nameInput) => {
+        validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
+            console.log('Please enter a name.');
             return false;
           }
         }
       },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'What is the ID number of the individual?',
-        when: ({ type }) => type != 'Quit Application?',
-        validate: (idInput) => {
-          if (idInput) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is the email address of the individual?',
-        when: ({ type }) => type != 'Quit Application?',
-        validate: (emailInput) => {
-          if (emailInput) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'officeNum',
-        message: 'What is the office number of the manager?',
-        when: ({ type }) => type === 'Manager',
-        validate: (officeNumInput) => {
-          if (officeNumInput) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'What is the GitHub username of the engineer?',
-        when: ({ type }) => type === 'Engineer',
-        validate: (githubInput) => {
-          if (githubInput) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'school',
-        message: 'What is the name of the school the intern attends?',
-        when: ({ type }) => type === 'Intern',
-        validate: (schoolInput) => {
-          if (schoolInput) {
-            return true;
-          } else {
-            return false;
-          }
-        }
-      }
-    )
-}
+      // {
+      //   type: 'input',
+      //   name: 'id',
+      //   message: 'What is the ID number of the individual?',
+      //   when: ({ type }) => type != 'Quit Application?',
+      //   validate: (idInput) => {
+      //     if (idInput) {
+      //       return true;
+      //     } else {
+      //       console.log('Please enter a id.');
+      //       return false;
+      //     }
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'email',
+      //   message: 'What is the email address of the individual?',
+      //   when: ({ type }) => type != 'Quit Application?',
+      //   validate: (emailInput) => {
+      //     if (emailInput) {
+      //       return true;
+      //     } else {
+      //       console.log('Please enter the email address.');
+      //       return false;
+      //     }
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'officeNum',
+      //   message: 'What is the office number of the manager?',
+      //   when: ({ type }) => type === 'Manager',
+      //   validate: (officeNumInput) => {
+      //     if (officeNumInput) {
+      //       return true;
+      //     } else {
+      //       console.log('Please enter the manger office number.');
+      //       return false;
+      //     }
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'github',
+      //   message: 'What is the GitHub username of the engineer?',
+      //   when: ({ type }) => type === 'Engineer',
+      //   validate: (githubInput) => {
+      //     if (githubInput) {
+      //       return true;
+      //     } else {
+      //       console.log('Please enter a GitHub username.');
+      //       return false;
+      //     }
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'school',
+      //   message: 'What is the name of the school the intern attends?',
+      //   when: ({ type }) => type === 'Intern',
+      //   validate: (schoolInput) => {
+      //     if (schoolInput) {
+      //       return true;
+      //     } else {
+      //       console.log('Please enter the school attended.');
+      //       return false;
+      //     }
+      //   }
+      // }
+    
+    ]
 
-newEmployeePrompt();
+inquirer.prompt(newEmployeePrompt);
